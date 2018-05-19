3cbe382cdaefc9a036525486aee9d060073e2cb1
Marat Valiev <mz.valiev@gmail.com>
2018-05-18 21:54:48 -0400

    New db connection

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/todo-api');
+mongoose.connect('mongodb://admin:1234567@ds023475.mlab.com:23475/todo-db');
 
 
 mongoose.Promise = Promise;
